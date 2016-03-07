c5afc956ef9f419f1413e311932494a4bc227550
ndarshanrestless <darshan@restlessbandit.com>
2016-03-07 03:54:47 +0000

    create user fun

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,19 +1,20 @@
 # all the imports
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
-         abort, render_template, flash
+         abort, render_template, flash, Response
 from contextlib import closing
-
-
-# configuration
-DATABASE = '/ebs/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY = 'restless123'
-USERNAME = 'bandit'
-PASSWORD = 'restless'
+import ipdb
+from flask.json import jsonify
 
 # create our little application :)
 app = Flask(__name__)
+app.config.update(dict(
+    DATABASE='/ebs/tmp/flaskr.db',
+    DEBUG=True,
+    SECRET_KEY='restless123',
+    USERNAME='bandit',
+    PASSWORD='restless',
+))
 app.config.from_object(__name__)
 
 
@@ -28,6 +29,15 @@ def init_db():
         db.commit()
 
 
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+        return g.sqlite_db
+
+
 @app.before_request
 def before_request():
     g.db = connect_db()
@@ -40,8 +50,43 @@ def teardown_request(exception):
         db.close()
 
 
+@app.route('/', strict_slashes=False)
+def root_page():
+    return Response("What are you looking at?", 200)
+
+
+@app.route('/test', methods=['GET'])
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    j = {'key1': 'value1', 'key2': 'value2'}
+    return jsonify(j)
+
+
+@app.route('/createuser', methods=['POST'])
+def create_user():
+    """                                                                                                           {                                                                                                            "username": "naren",                                                                                         "password": "welcome",                                                                                       "doctor-type": "Some Dr speciality"                                                                          }                                                                                                            """
+    jd = request.get_json()
+
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+               [jd['username'], jd['password']])
+    db.commit()
+return Response("Created user succesfully!", 200)
+
+
+@app.route('/showusers, methods=['GET']')
+def show_user_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+return jsonify(entries)
+
+
+@app.route('/add', methods=['POST'])
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=3000)
+
 
 '''
 That way someone can set an environment variable called FLASKR_SETTINGS
